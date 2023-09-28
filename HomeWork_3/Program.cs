0e9462c9418f91947ad15b2314a2b5b0bad14e6a
AndreyT <shax0491@gmail.com>
2023-09-28 23:47:47 +0300

    Задача 3

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+static void Main()
+{
+int[] array = new int[8];
+Random random = new Random();
+
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = random.Next(1, 99);
+    }
+
+    foreach (int num in array)
+    {
+        Console.Write(num + " ");
+    }
+    Console.ReadLine();
+}
+}
